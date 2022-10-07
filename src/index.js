619083997dcc2839340690e5c88a15b0f83c0703
kelvin <kelvinices@gmail.com>
2022-10-07 18:24:46 +0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import { Viewer } from 'photo-sphere-viewer';
+import { MarkersPlugin } from 'photo-sphere-viewer/dist/plugins/markers';
+import { VideoPlugin } from 'photo-sphere-viewer/dist/plugins/video';
+import { EquirectangularVideoAdapter } from 'photo-sphere-viewer/dist/adapters/equirectangular-video';
+
+// testing show video
+// const viewer = new Viewer({
+//   adapter: EquirectangularVideoAdapter,
+//   container: document.querySelector('#viewer'),
+  
+//    panorama: {
+//      source: 'public/asset/Ayutthaya_HD.mp4',
+//    },
+//   plugins: [
+//     [VideoPlugin, MarkersPlugin],
+//   ],
+// });
+
+const viewer = new Viewer({
+  adapter: EquirectangularVideoAdapter,
+  container: document.querySelector('#viewer'),
+  panorama: {
+    source: 'public/asset/Ayutthaya_HD.mp4',
+  },
+  plugins: [
+    [VideoPlugin, MarkersPlugin, {
+      markers: [
+        {
+          id: 'new-marker',
+          longitude: 0,
+          latitude: 0,
+          image: 'public/asset/pin-blue.png',
+          width: 32,
+          height: 32,
+        },
+      ],
+    }],
+  ],
+});
+
+const markersPlugin = viewer.getPlugin(MarkersPlugin);
+
+markersPlugin.on('select-marker', (e, marker) => {
+  markersPlugin.updateMarker({
+    id: marker.id,
+    image: 'public/asset/pin-blue.png'
+  });
+  
+});
+
+viewer.on('click', (e, data) => {
+  if (!data.rightclick) {
+    markersPlugin.addMarker({
+      id: '#' + Math.random(),
+      longitude: data.longitude,
+      latitude: data.latitude,
+      image: 'public/asset/pin-blue.png',
+      width: 32,
+      height: 32,
+      anchor: 'bottom center',
+      tooltip: 'Generated pin',
+      data: {
+        generated: true
+      }
+    });
+  }
+});
+
+
+
