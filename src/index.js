4744d8ebe4a25cab5bd02fd0edf2d1de645c0dfd
kelvin <kelvinices@gmail.com>
2022-10-07 18:30:40 +0700

    change panorama mode

--- src/index.js
+++ src/index.js
@@ -3,40 +3,49 @@ import { MarkersPlugin } from 'photo-sphere-viewer/dist/plugins/markers';
 import { VideoPlugin } from 'photo-sphere-viewer/dist/plugins/video';
 import { EquirectangularVideoAdapter } from 'photo-sphere-viewer/dist/adapters/equirectangular-video';
 
+
+const viewer = new Viewer({
+  container: document.querySelector('#viewer'),
+  panorama: '/public/asset/i002.jpg',
+  plugins: [
+    [MarkersPlugin, {
+      markers: [
+        {
+          id: 'marker1',
+          longitude: 0,
+          latitude: 0,
+          image: 'public/asset/marker.png',
+          width: 32,
+          height: 32,
+          anchor: 'bottom center',
+          tooltip: 'Marker 1'},],
+        }
+      ],
+  ]
+});
+
 // testing show video
 // const viewer = new Viewer({
 //   adapter: EquirectangularVideoAdapter,
 //   container: document.querySelector('#viewer'),
-  
-//    panorama: {
-//      source: 'public/asset/Ayutthaya_HD.mp4',
-//    },
+//   panorama: {
+//     source: 'public/asset/Ayutthaya_HD.mp4',
+//   },
 //   plugins: [
-//     [VideoPlugin, MarkersPlugin],
+//     [VideoPlugin, MarkersPlugin, {
+//       markers: [
+//         {
+//           id: 'new-marker',
+//           longitude: 0,
+//           latitude: 0,
+//           image: 'public/asset/pin-blue.png',
+//           width: 32,
+//           height: 32,
+//         },
+//       ],
+//     }],
 //   ],
 // });
-
-const viewer = new Viewer({
-  adapter: EquirectangularVideoAdapter,
-  container: document.querySelector('#viewer'),
-  panorama: {
-    source: 'public/asset/Ayutthaya_HD.mp4',
-  },
-  plugins: [
-    [VideoPlugin, MarkersPlugin, {
-      markers: [
-        {
-          id: 'new-marker',
-          longitude: 0,
-          latitude: 0,
-          image: 'public/asset/pin-blue.png',
-          width: 32,
-          height: 32,
-        },
-      ],
-    }],
-  ],
-});
 
 const markersPlugin = viewer.getPlugin(MarkersPlugin);
 
